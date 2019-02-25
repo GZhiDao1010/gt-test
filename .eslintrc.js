8b33bed1718fade5a690f7ddece16a52ebf440e9
GZhiDao1010 <GZhiDao1010@gmail.com>
2019-02-25 17:24:12 +0800

    Add files via upload

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    root: true,
+    env: {
+        node: true
+    },
+    'extends': [
+        'plugin:vue/essential',
+        '@vue/standard'
+    ],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'indent': [
+            'warn', 4, {
+                'SwitchCase': 1
+            }
+        ],
+        'eol-last': 'off',
+        'space-before-function-paren': 'off',
+        'space-before-keywords': 'off',
+        'space-in-parens': 'off',
+        'func-call-spacing': 'off',
+        'no-multiple-empty-lines': 'off',
+        'semi': 'off',
+        'padded-blocks': 'off'
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    }
+}
